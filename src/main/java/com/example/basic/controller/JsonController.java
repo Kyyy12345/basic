9a0ac837b48a278e724dee0da9ba81d822a8cb1f
Kyyy12345 <rladbsduf135@naver.com>
2026-06-23 17:13:55 +0900

    RequestParam 요청과 Json 응답 연습

--- src/main/java/com/example/basic/controller/JsonController.java
+++ src/main/java/com/example/basic/controller/JsonController.java
@@ -1,8 +1,11 @@
 package com.example.basic.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.basic.ExamResponse;
@@ -20,5 +23,18 @@ public class JsonController {
         response.setList(Arrays.asList(u1, u2));
 
         return response;
+    }
+
+    @GetMapping("/req/data")
+    public Map<String, String> data(@RequestParam(required = false) String area,
+            @RequestParam(required = false) String score) {
+        Map map = new HashMap<>();
+        if (area != null) {
+            map.put("area", area);
+        }
+        if (score != null) {
+            map.put("score", score);
+        }
+        return map;
     }
 }
